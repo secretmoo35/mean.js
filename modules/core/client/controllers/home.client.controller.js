6a20ec42a88240a33135a31620ffe9f906c4b671
secretmoo35 <sarayut-moo.th@hotmail.com>
2016-11-10 00:04:09 +0700

    cicle menu

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -2,8 +2,13 @@
 
 angular.module('core').controller('HomeController', ['$scope', 'Authentication',
   function ($scope, Authentication) {
+    // menu 
+    $scope.circleMenu = function () {
+      var el = '.js-menu';
+      var myMenu = cssCircleMenu(el);
+    }
     // This provides Authentication context.
     $scope.authentication = Authentication;
-    
+
   }
 ]);
