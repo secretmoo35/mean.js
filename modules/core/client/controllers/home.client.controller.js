649ec8c068d91a71ddeca096f7b60ffd62e208ca
secretmoo35 <sarayut-moo.th@hotmail.com>
2016-11-10 16:20:28 +0700

    test menu

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -6,17 +6,11 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
     $scope.loadMenu = function () {
       // var el = '.js-menu';
       // var myMenu = cssCircleMenu(el);
-      var btn = document.querySelector('.nav__button'),
-        container = document.querySelector('.container');
-
-      btn.addEventListener('click', function () {
-        this.classList.toggle('open');
-        container.classList.toggle('active');
-      });
 
     }
     // This provides Authentication context.
     $scope.authentication = Authentication;
+
 
   }
 ]);
