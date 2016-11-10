d16ffffd51be163132669075d974c8e53c681d12
secretmoo35 <sarayut-moo.th@hotmail.com>
2016-11-10 16:42:41 +0700

    test menu

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -3,11 +3,14 @@
 angular.module('core').controller('HomeController', ['$scope', 'Authentication',
   function ($scope, Authentication) {
     // menu 
-    $scope.loadMenu = function () {
-      // var el = '.js-menu';
-      // var myMenu = cssCircleMenu(el);
+    var btn = document.querySelector('.nav__button'), 
+    container = document.querySelector('.container');
 
-    }
+    btn.addEventListener('click', function () {
+      this.classList.toggle('open');
+      container.classList.toggle('active');
+    });
+
     // This provides Authentication context.
     $scope.authentication = Authentication;
 
